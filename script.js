441e34c03f8e95de8eedba00b76238e18ec7607b
Sahil Faniband <sahilfaniband9602@gmail.com>
2024-06-22 00:31:39 +0530

    update on form and some miner changes

---
+++ script.js
@@ -0,0 +1,22 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const status = document.getElementById('status');
+    const form = event.target;
+
+    status.textContent = 'Sending...';
+
+    emailjs.sendForm('service_vnec8f4', 'template_mxzhr3m', this)
+        .then(function() {
+            status.textContent = 'Message sent successfully!';
+            form.reset();
+            setTimeout(function() {
+                status.textContent = '';
+            }, 5000)
+        }, function(error) {
+            console.error('Failed to send the message:', error);
+            alert('Failed to send the message. Please try again later.');
+        
+        });
+});
+
